463ecb42bfe3572514f2c78e9344e30a446da40d
Marwan-Mostafa <marwan.mf.tech@gmail.com>
2026-05-14 03:16:11 +1100

    update the search input

--- js/components/searchInput.js
+++ js/components/searchInput.js
@@ -1,24 +1,37 @@
 
 export function createSearchHandler({renderResults, results, loader, errorBox}){
+
+    let controller
+
     return async function(e) {
         const value = e.target.value
 
-        
+        errorBox.textContent = ""
+
         if(!value.trim()){
             results.innerHTML = ""
+            loader.classList.add("hidden")
             return
         }
 
         try{
+            if(controller) controller.abort()
+            controller = new AbortController()
+
             loader.classList.remove("hidden");
             
-            const res = await fetch(`https://dummyjson.com/products/search?q=${value}`)
+            const res = await fetch(`https://dummyjson.com/products/search?q=${value}`, { signal: controller.signal })
+            
+            if(!res.ok){
+                throw new Error("API Error")
+            }
             const data = await res.json()
 
             renderResults(data.products, results)
         }   
         catch(err){
-            errorBox.textContent = "Something is wrong"
+            if(err.name !== "AbortError")
+                errorBox.textContent = "Something is wrong"
         }
         finally{
             loader.classList.add("hidden");
